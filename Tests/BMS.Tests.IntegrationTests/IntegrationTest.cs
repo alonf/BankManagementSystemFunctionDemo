f29379c5567062d7b076ab296fd2c7e9c91e5f56
Alon Fliess <alonf@codevalue.net>
2022-10-19 13:52:26 +0300

    Add tests

--- Tests/BMS.Tests.IntegrationTests/IntegrationTest.cs
+++ Tests/BMS.Tests.IntegrationTests/IntegrationTest.cs
@@ -22,7 +22,7 @@ namespace BMS.Tests.IntegrationTests
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
             WriteIndented = true
         };
-    
+
         private async Task<string> RegisterTestAccountAsync(string email)
         {
             var customerRegistrationInfo = new CustomerRegistrationInfo()
@@ -87,14 +87,14 @@ namespace BMS.Tests.IntegrationTests
 
             return balanceInfo;
         }
-        
+
         [Fact]
         public async Task TestDeposit()
         {
             var accountId = await RegisterTestAccountAsync("deposit@email.com");
 
             var startBalanceInfo = await GetAccountBalanceAsync(accountId);
-            
+
             var accountTransactionInfo = new AccountTransactionInfo()
             {
                 CallerId = "Teller1",
@@ -178,22 +178,50 @@ namespace BMS.Tests.IntegrationTests
             var json = JsonSerializer.Serialize(accountTransactionInfo, SerializeOptions);
             var data = new StringContent(json, Encoding.UTF8, "application/json");
 
-            await _signalR.StartSignalR();
             var response = await _httpClient.PostAsync("Withdraw", data);
             Assert.NotNull(response);
-            Assert.Equal(200, (int)response.StatusCode);
+            Assert.Equal(400, (int)response.StatusCode);
 
-            var result = await _signalR.WaitForSignalREventAsync();
-            Assert.True(result);
-            Assert.NotEmpty(_signalR.Messages);
-            
-            Assert.Contains(accountTransactionInfo.RequestId, _signalR.Messages.Select(e => e.RequestId));
-            Assert.False(_signalR.Messages.Where(e => e.RequestId == accountTransactionInfo.RequestId).First().IsSuccessful);
-                
             var endBalanceInfo = await GetAccountBalanceAsync(accountId);
-
             Assert.Equal(startBalanceInfo.Balance, endBalanceInfo.Balance);
+        }
 
+        [Fact]
+        public async Task TestTransactionHistory()
+        {
+            var accountId = await RegisterTestAccountAsync("transaction@email.com");
+
+            for (int i = 1; i <= 6; ++i)
+            {
+                var accountTransactionInfo = new AccountTransactionInfo()
+                {
+                    CallerId = "Teller1",
+                    RequestId = Guid.NewGuid().ToString(),
+                    SchemaVersion = "1.0",
+                    AccountId = accountId,
+                    Amount = i * 100
+                };
+
+                var json = JsonSerializer.Serialize(accountTransactionInfo, SerializeOptions);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+                await _signalR.StartSignalR();
+                var response = await _httpClient.PostAsync(i % 2 == 0 ? "Deposit" : "Withdraw", data);
+                Assert.NotNull(response);
+                Assert.Equal(200, (int)response.StatusCode);
+
+                var result = await _signalR.WaitForSignalREventAsync();
+                Assert.True(result);
+                Assert.NotEmpty(_signalR.Messages);
+                Assert.Contains(accountTransactionInfo.RequestId, _signalR.Messages.Select(e => e.RequestId));
+                Assert.True(_signalR.Messages.Where(e => e.RequestId == accountTransactionInfo.RequestId).First().IsSuccessful);
+            }
+
+            var responseHistory = await _httpClient.GetAsync($"GetAccountTransactionHistory?accountId={accountId}");
+            var transactionHistory = JsonSerializer.Deserialize<AccountTransactionResponse[]>(await responseHistory.Content.ReadAsStringAsync(), SerializeOptions);
+            Assert.NotNull(transactionHistory);
+            Assert.Equal(6, transactionHistory.Length);
+            Assert.Equal(300, transactionHistory.Sum(e => e.TransactionAmount));
         }
     }
 }
